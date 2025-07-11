404d5c4d260b95d9a55943f7847ccffa37231379
SadiaKhalil125 <sadiakhalil957@gmail.com>
2025-07-11 19:50:35 +0500

    final

--- todoapp/backend/server.js
+++ todoapp/backend/server.js
@@ -15,6 +15,14 @@ app.use(express.json());
 
 // Routes
 app.use('/api/todos', todoRoutes);
+// Serve static files from the React frontend build
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Serve React for any other route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
